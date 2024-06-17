cf91956f9aa93c5ea5d3b1416963744816906cfa
jkousemaker <70757540+jkousemaker@users.noreply.github.com>
2024-06-17 21:47:41 +0200

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,50 +1,19 @@
-import { Canvas, useThree, useFrame } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
+
+import { Suspense } from "react";
 
 import Curve from "@/components/Layout/Curve";
-
+import Scene from "@/Scene";
 export default function Home() {
   return (
     <>
       <Curve backgroundColor="#B0AD98">
-        <h1>Home</h1>
-        <div className="flex flex-col ">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-            imperdiet nibh sit amet velit dignissim, non tempus nisl
-            pellentesque. Praesent sagittis magna sit amet ex blandit, id
-            pharetra lectus feugiat. Praesent sit amet congue ipsum, in ultrices
-            neque. In dapibus in purus vitae dignissim. Quisque molestie
-            ullamcorper elementum. Sed sodales erat augue. Lorem ipsum dolor sit
-            amet, consectetur adipiscing elit. Duis aliquet quis lectus vitae
-            venenatis. Aliquam erat volutpat. Nulla maximus sodales nibh dapibus
-            congue. Integer nec pharetra felis, quis commodo elit. Fusce et
-            aliquet neque. Vivamus leo diam, pharetra ut lorem eu, suscipit
-            egestas ipsum. Aenean mauris ligula, laoreet ut volutpat sit amet,
-            convallis et turpis.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-            imperdiet nibh sit amet velit dignissim, non tempus nisl
-            pellentesque. Praesent sagittis magna sit amet ex blandit, id
-            pharetra lectus feugiat. Praesent sit amet congue ipsum, in ultrices
-            neque. In dapibus in purus vitae dignissim. Quisque molestie
-            ullamcorper elementum. Sed sodales erat augue. Lorem ipsum dolor sit
-            amet, consectetur adipiscing elit. Duis aliquet quis lectus vitae
-            venenatis. Aliquam erat volutpat. Nulla maximus sodales nibh dapibus
-            congue. Integer nec pharetra felis, quis commodo elit. Fusce et
-            aliquet neque. Vivamus leo diam, pharetra ut lorem eu, suscipit
-            egestas ipsum. Aenean mauris ligula, laoreet ut volutpat sit amet,
-            convallis et turpis.
-          </p>
-          <p>
-            Quisque molestie ullamcorper elementum. Sed sodales erat augue.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            aliquet quis lectus vitae venenatis. Aliquam erat volutpat. Nulla
-            maximus sodales nibh dapibus congue. Integer nec pharetra felis,
-            quis commodo elit. Fusce et aliquet neque. Vivamus leo diam,
-            pharetra ut lorem eu, suscipit egestas ipsum. Aenean mauris ligula,
-            laoreet ut volutpat sit amet, convallis et turpis.
-          </p>
+        <div className="fixed inset-0 size-full z-[9999]">
+          <Canvas>
+            <Suspense fallback={null}>
+              <Scene />
+            </Suspense>
+          </Canvas>
         </div>
       </Curve>
     </>
