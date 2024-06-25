944bc6543d4e1088593ea65cfe4eafcd439048c7
jkousemaker <70757540+jkousemaker@users.noreply.github.com>
2024-06-26 00:20:16 +0200

    Update index.jsx

--- src/components/Elements/InputField.jsx/index.jsx
+++ src/components/Elements/InputField.jsx/index.jsx
@@ -1,7 +1,7 @@
 export default function InputField({ type, name, placeholder }) {
   return (
     <div className="flex flex-col items-start">
-      <label for={name}>{placeholder}</label>
+      <label htmlFor={name}>{placeholder}</label>
       <input
         onBeforeInput={() => console.log(true)}
         type={type}
